d039dcf088192434bf35a9020220ed10669b121b
Btate712 <btate712@gmail.com>
2019-06-09 10:59:49 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  document.addEventListener('keydown', onKeyDownHandler);
+  document.querySelector('body').addEventListener('keydown', onKeyDownHandler);
 }
 
 function onKeyDownHandler(e) {
@@ -11,6 +11,7 @@ function onKeyDownHandler(e) {
 
   if (key === code[index]) {
     index++;
+    console.log(index);
 
     if (index === code.length) {
       alert("Hurray!");
